146c1c71ab4b5dd36efc266e9561e11b32df88bc
MrShifu01 <stander.christian.com>
2023-07-03 15:22:12 +0200

    a

--- client/src/tests/AdminPage.test.js
+++ client/src/tests/AdminPage.test.js
@@ -3,22 +3,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../redux/store";
 import AdminPage from "../pages/AdminPage";
-import { setClients } from "../redux/clientsSlice";
-import { setLoans } from "../redux/loansSlice";
 
 jest.mock("axios", () => ({
   get: jest.fn(() => Promise.resolve({ data: [] })),
 }));
 
 test("Clicking on 'Edit User Permissions' button opens the modal", async () => {
-
-  // Mock the data to be returned by the axios.get calls
-  const clientsData = [{ id: 1, name: "User 1" }, { id: 2, name: "User 2" }];
-  const loansData = [{ id: 1, amount: 1000 }, { id: 2, amount: 2000 }];
-
-  // Set up the initial state of the store
-  store.dispatch(setClients(clientsData));
-  store.dispatch(setLoans(loansData));
 
   // Render the component
   render(
