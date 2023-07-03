0a9a20cbb2289f430b3f62e730ffc6035c957de3
MrShifu01 <stander.christian.com>
2023-07-03 14:09:59 +0200

    not doing clerk for this project

--- client/src/App.js
+++ client/src/App.js
@@ -7,29 +7,21 @@ import Layout from './components/Layout';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import ChangePasswordPage from './pages/ChangePasswordPage';
-import { ClerkProvider } from "@clerk/clerk-react";
 
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
 
-if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-
 function App() {
   return (
-    <ClerkProvider publishableKey={clerkPubKey}>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index={true} path='/' element={<IndexPage/>}/>
-          <Route path='/admin' element={<AdminPage/>}/>
-          <Route path='/register' element={<RegisterPage/>}/>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/change-password' element={<ChangePasswordPage/>}/>
-        </Route>
-      </Routes>
-    </ClerkProvider>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index={true} path='/' element={<IndexPage/>}/>
+        <Route path='/admin' element={<AdminPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/change-password' element={<ChangePasswordPage/>}/>
+      </Route>
+    </Routes>
   );
 }
 
